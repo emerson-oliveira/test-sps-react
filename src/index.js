54b50e0c05a15cd3c6a94a4b2f837bf136510f79
Emerson Oliveira <emerson-ods@hotmail.com>
2025-03-17 09:00:00 +0000

    refactor: restructure entry point by moving React rendering logic to index.jsx and removing routes.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import router from "./routes";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+// Este arquivo serve apenas como ponto de entrada para o create-react-app
+// que espera um arquivo index.js por padrão
+import "./index.jsx";
